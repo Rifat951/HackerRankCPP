a86c82758810cafa0d32314f7902e0c9a5818f27
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2023-02-14 15:17:32 +1030

    Create ConditionalStatements.cpp

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    map <int, string> m = {{1, "one"}, {2, "two"},
+    {3, "three"},
+    {4, "four"},
+    {5, "five"},
+    {6, "six"},
+    {7, "seven"},
+    {8, "eight"},
+    {9, "nine"},
+     };
+     // creating iterator 
+    map < int, string> :: iterator itr;
+    itr = m.find(n);
+   if(itr != m.end()){
+           cout << itr -> second;
+    }else { cout << "Greater than 9";}
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
