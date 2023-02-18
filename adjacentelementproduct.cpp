b4e82c1f1092e57109fe078700cd8b9c18e56d01
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2023-02-18 20:01:26 +1030

    From CodeSignal

---
+++ adjacentelementproduct.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+
+int solution(vector<int> inputArray) {
+
+    // traverse array
+    
+    //make pair
+    //product the pairs
+    // display output
+    
+   // sort(inputArray.begin(), inputArray.end());
+    if ( !inputArray.empty()){
+        
+    
+       vector<int>::const_iterator end = inputArray.cend() - 1;
+       int temp ;
+       int currtemp ;
+       int result = 0;
+       for (vector<int>::const_iterator it = ainputArray.cbegin(); it != end; ++it){
+             //cout << '(' << *it << ',' << *(it+1) << ')' << " -- ";
+            temp = (*it * *(it+1));
+            // cout << "temp" << temp << endl;;
+            if(temp > currtemp){
+                currtemp = temp;
+            }
+            else {
+                temp = temp;
+            }
+   
+        }
+        result = max(temp,currtemp);
+        
+        return result;
+    
+    }
+    
+     
+}
